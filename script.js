c65ec3f5b4a9f0a7b72c7141c7339c8eabd7342b
DURGESH KUMAR <68071906+aditya4-4@users.noreply.github.com>
2023-08-26 23:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
   // Write your code here
+	let binary = "";
+	while(num > 0){
+		if(num % 2 == 1){
+			binary += "1";
+		}
+		else{
+			binary += "0"
+		}
+		num = Math.floor(num / 2);
+	}
+	console.log(binary.reverse())
 }
 
 window.decimalToBinary = decimalToBinary;
